387086fc9a04b89bc304ff7e805cb3dacda001e3
Md Hasib <md.hasibuzzaman001@google.com>
2022-04-07 17:44:37 +0600

    spelling mistake

--- determint_even_odd_input.cpp
+++ determint_even_odd_input.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     //Taking input form the user
     int number;
-    cout << "Inout a number: ";
+    cout << "Input a number: ";
     cin >> number;
 
     //checking the number is even or odd
